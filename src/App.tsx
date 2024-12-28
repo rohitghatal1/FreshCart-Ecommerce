b1dbde843cb4a487b68f8f69c6c7a116db85681a
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2024-12-28 15:38:54 +0545

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import MainScreen from "./components/MainScreen";
 
 function App() {
   return (
     <>
-      <MainScreen />
+      <Routes>
+        <Route path="/*" element={<MainScreen />}></Route>
+      </Routes>
     </>
   );
 }
