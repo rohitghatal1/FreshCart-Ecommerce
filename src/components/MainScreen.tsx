abd903b6993710467f5af5031f8f5a9737d16af6
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2024-12-28 15:36:06 +0545

    added mainscreen and home page

---
+++ src/components/MainScreen.tsx
@@ -0,0 +1,15 @@
+import Header from "./header/Header";
+
+const MainScreen: React.FC = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <Header />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MainScreen;
---
+++ src/components/pages/Homepage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homepage: React.FC = () => {
+  return (
+    <>
+      <h2>Home Page</h2>
+    </>
+  );
+};
+
+export default Homepage;
