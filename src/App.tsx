a73d9c78e6a382b79758e8e121a7b881ca0fdc91
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2024-12-29 20:50:49 +0545

    added meat and sea food component and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import MainScreen from "./components/MainScreen";
+import Homepage from "./components/pages/Homepage";
+import MeatSeaFood from "./components/pages/MeatSeaFood";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/*" element={<MainScreen />}></Route>
+        <Route path="/*" element={<MainScreen />}>
+          <Route path="/dashboard" element={<Homepage />} />
+          <Route path="/meat-sea-food" element={<MeatSeaFood />} />
+        </Route>
       </Routes>
     </>
   );
