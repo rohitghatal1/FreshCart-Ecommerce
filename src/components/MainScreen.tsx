c37e037ca717e8afae8c74cefebeafefc5834c91
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-01-01 20:35:41 +0545

    added dynamic tab dsipaly code

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -1,11 +1,29 @@
 import { useLocation } from "react-router-dom";
 import Header from "./header/Header";
 import Homepage from "./pages/Homepage";
+import MeatSeaFood from "./pages/MeatSeaFood";
+import Contacts from "./pages/Contacts";
+import Beverages from "./pages/Beverages";
+import Bakery from "./pages/Bakery";
 
 const MainScreen: React.FC = () => {
   const location = useLocation();
   const renderContent = () => {
-    return "Content";
+    switch (location.pathname) {
+      case "/":
+        return <Homepage />;
+      case "/meat-sea-food":
+        return <MeatSeaFood />;
+
+      case "/bakery":
+        return <Bakery />;
+
+      case "beverages":
+        return <Beverages />;
+
+      case "/contacts":
+        return <Contacts />;
+    }
   };
   return (
     <>
