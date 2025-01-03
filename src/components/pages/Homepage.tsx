57caa7cc6b51d5b59936e7027e2ab7b0acfcf559
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-01-03 22:38:25 +0545

    added product cards

--- src/components/pages/Homepage.tsx
+++ src/components/pages/Homepage.tsx
@@ -3,7 +3,12 @@ import React from "react";
 const Homepage: React.FC = () => {
   return (
     <>
-      <h2>Home Page</h2>
+      <div>
+        <h2>Products available</h2>
+        <div className="flex items-center justify-between gap-2 p-4">
+          <div className="border w-60 rounded-md p-3"></div>
+        </div>
+      </div>
     </>
   );
 };
