cc5ff49106f4185d87aa31ae3f31ac49f858fccb
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-01-04 21:36:15 +0545

    added styles

--- src/components/pages/Contacts.tsx
+++ src/components/pages/Contacts.tsx
@@ -4,11 +4,11 @@ const Contacts: React.FC = () => {
       <div>
         <h2 className="font-semibold text-lg">Contacts</h2>
         <div className="flex items-center justify-between gap-4">
-          <div>
-            <p>Left Section</p>
+          <div className="border p-3 rounded-md">
+            <p className="text-center font-semibold">Left Section</p>
           </div>
-          <div>
-            <p>Right Section</p>
+          <div className="border p-3 rounded-md">
+            <p className="text-center font-semibold">Right Section</p>
           </div>
         </div>
       </div>
