6ed71acd38ab26430387249ff504af5e00c9f5b5
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-01-04 21:34:43 +0545

    added contact page

--- src/components/pages/Contacts.tsx
+++ src/components/pages/Contacts.tsx
@@ -1,7 +1,17 @@
 const Contacts: React.FC = () => {
   return (
     <>
-      <h2>Contact Page</h2>
+      <div>
+        <h2 className="font-semibold text-lg">Contacts</h2>
+        <div className="flex items-center justify-between gap-4">
+          <div>
+            <p>Left Section</p>
+          </div>
+          <div>
+            <p>Right Section</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
