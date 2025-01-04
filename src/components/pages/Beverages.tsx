c8913d301a78509d6c01f771f475a884b45564d5
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-01-04 21:37:15 +0545

    added beverages page

--- src/components/pages/Beverages.tsx
+++ src/components/pages/Beverages.tsx
@@ -1,7 +1,8 @@
 const Beverages: React.FC = () => {
   return (
     <>
-      <h2>Beverages page</h2>
+      <h2 className="font-semibold text-lg">Beverages page</h2>
+      <div className="p-4 mt-4"></div>
     </>
   );
 };
