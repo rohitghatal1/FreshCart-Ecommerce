36ad5b3de56acdfca209dba9d1ae6dc1a913b411
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-01-05 20:22:11 +0545

    added meat section

--- src/components/pages/MeatSeaFood.tsx
+++ src/components/pages/MeatSeaFood.tsx
@@ -1,7 +1,14 @@
 const MeatSeaFood = () => {
   return (
     <>
-      <h2>Mead and Sea Food Component</h2>
+      <div className="mt-3 p-3">
+        <h2 className="text-lg font-semibold py-2">
+          Mead and Sea Food Component
+        </h2>
+        <div>
+          <h2 className="text-lg font-semibold text-center py-2">Meats:</h2>
+        </div>
+      </div>
     </>
   );
 };
