758d248879e2443b6edcc641ce3a8dd247aa22fe
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-01-05 20:30:51 +0545

    added contact form

--- src/components/pages/Contacts.tsx
+++ src/components/pages/Contacts.tsx
@@ -1,14 +1,40 @@
+import { Button, Form, Input } from "antd";
+
 const Contacts: React.FC = () => {
   return (
     <>
       <div>
         <h2 className="font-semibold text-lg">Contacts</h2>
-        <div className="flex items-center justify-between gap-4">
+        <div className="flex items-center gap-4">
           <div className="border p-3 rounded-md">
-            <p className="text-center font-semibold">Left Section</p>
+            <figure>
+              <img src="#" alt="" />
+            </figure>
+            <p>Rohit Ghatal</p>
           </div>
           <div className="border p-3 rounded-md">
-            <p className="text-center font-semibold">Right Section</p>
+            <h3 className="text-center font-semibold">Contact me</h3>
+            <div>
+              <Form>
+                <Form.Item label="Name">
+                  <Input />
+                </Form.Item>
+                <Form.Item label="Subject">
+                  <Input />
+                </Form.Item>
+                <Form.Item label="Message">
+                  <Input />
+                </Form.Item>
+                <Form.Item label="Email">
+                  <Input />
+                </Form.Item>
+                <Form.Item>
+                  <Button type="primary" size="large">
+                    Send Message
+                  </Button>
+                </Form.Item>
+              </Form>
+            </div>
           </div>
         </div>
       </div>
