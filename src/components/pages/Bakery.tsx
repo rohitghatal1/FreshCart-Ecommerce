5a030c2f05a722bfee2191f8c709cc73c22a2961
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-01-08 21:02:20 +0545

    updated styles for item cards

--- src/components/pages/Bakery.tsx
+++ src/components/pages/Bakery.tsx
@@ -3,9 +3,9 @@ const Bakery: React.FC = () => {
     <>
       <h2>Available Items:</h2>
       <div className="flex items-center gap-3">
-        <div className="border rounded-md p-2">
-          <img src="#" alt="" />
-          <p>Dought Nut</p>
+        <div className="border rounded-md p-2 w-48">
+          <img src="#" alt="" className="h-4/5 w-full" />
+          <p className="py-2 text-center">Dought Nut</p>
         </div>
       </div>
     </>
